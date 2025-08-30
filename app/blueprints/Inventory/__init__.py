ac8e3227e0ad96aafcb3855ca7f3112fb02797f3
Joshua D Smith <2smithjosh@gmail.com>
2025-08-29 23:36:26 -0700

    Fix case: blueprints/inventory; add __init__.py; ensure inventory_bp

--- app/blueprints/Inventory/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Blueprint
-
-inventory_bp = Blueprint("inventory", __name__)
-
-from . import routes  # ensure routes are loaded
-from . import schemas  # ensure schemas are loaded
