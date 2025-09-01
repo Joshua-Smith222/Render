99a25fc1c4f38788abe2b468dc5c89206a61eeb0
Joshua D Smith <2smithjosh@gmail.com>
2025-08-31 23:51:19 -0700

    trigger deploy

--- app/__init__.py
+++ app/__init__.py
@@ -190,6 +190,6 @@ def create_app(overrides: dict | None = None) -> Flask:
 
     @app.get("/")
     def root():
-        return redirect(url_for("swagger_ui.swagger_ui"))
+        return redirect(url_for("/docs", code =302))
 
     return app
